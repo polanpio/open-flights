b5e3d0e82f5579038dd89b20493367d19eb08008
Piotr Polanski <polanski.pio@gmail.com>
2021-11-28 16:00:14 +0900

    added models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: 'Great airline',
+    description: 'Great time there',
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: 'Awful line',
+    description: 'The kicked out a passenger mid-flight',
+    score: 1,
+    airline: airlines.first
+  }
+])
