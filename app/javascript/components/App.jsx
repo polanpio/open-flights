87bd13fb1b53bb0b83b4090f2381453a9949fdae
Piotr Polanski <polanski.pio@gmail.com>
2021-11-30 21:53:07 +0900

    playing with routes

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Airlines from './Airlines'
+import Airline from './Airline'
 
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <Routes>
+      <Route exact path="//*" element={<Airlines />} />
+      <Route exact path="/airlines/:slug" element={<Airline />} />
+    </Routes>
   )
 }
 
