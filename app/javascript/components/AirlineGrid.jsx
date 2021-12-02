144f47d00b9593c7293429fca37800e1347857f6
Piotr Polanski <polanski.pio@gmail.com>
2021-12-02 21:07:06 +0900

    basic styles

--- app/javascript/components/AirlineGrid.jsx
+++ app/javascript/components/AirlineGrid.jsx
@@ -1,17 +1,64 @@
 import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`
+
+const AirlineLogo = styled.div`
+  height: 50px;
+  img {
+    height: 50px;
+    width: 50px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 100%;
+  }
+`
+
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`
+
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height:50px;
+  a {
+    color: #fff;
+    background-color: #71b406;
+    border-radius: 4px;
+    padding: 10px 50px;
+    cursor: pointer;
+    border-radius: 3px;
+    border: 1px solid #71b406;
+    text-align: center;
+    line-height: 20px;
+    min-height: 40px;
+    margin: 7px;
+    font-weight: 600;
+    text-decoration: none;
+    width: 100%;
+    transition: ease-in-out 0.1s;
+    &:hover{
+      border-color: #619a07;
+      background: #619a07;
+    }
+  }
+`
 
 const AirlineGrid = ({ attributes }) => {
   return (
-    <div className="card">
-      <div className="airline-logo">
+    <Card>
+      <AirlineLogo>
         <img src={attributes.image_url} alt={attributes.name} />
-      </div>
-      <div className="airline-name">{attributes.name}</div>
+      </AirlineLogo>
+      <AirlineName>{attributes.name}</AirlineName>
       <div className="airline-score">{attributes.avg_score}</div>
-      <div className="airline-link">
-        <a href={`/airlines/${attributes.slug}`}>View Airlines</a>
-      </div>
-    </div>
+      <LinkWrapper>
+        <Link to={`/airlines/${attributes.slug}`}>View Airlines</Link>
+      </LinkWrapper>
+    </Card>
   )
 }
 
