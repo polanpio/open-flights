c9c8a6213790dbd68284135d55106a47f52c49da
Piotr Polanski <polanski.pio@gmail.com>
2021-12-03 21:25:57 +0900

    routing issue, props in show

--- app/javascript/components/Airline.jsx
+++ app/javascript/components/Airline.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Airline = () => {
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(() => {
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+  }, [])
+
+  axios.get(url)
+    .then(res = console.log(res))
+    .catch(res = console.log(res))
   return <div>This is the Airlines#show view for this app</div>
 }
 
